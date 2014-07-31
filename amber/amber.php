3ccfca21043cfd1af91433d7ec7ad5093cbf2224
Jeffrey Licht <jeff@podconsulting.com>
2014-07-31 15:43:40 -0400

    Display cached content

--- amber/amber.php
+++ amber/amber.php
@@ -30,7 +30,10 @@ class Amber {
 	 */
 	private static function get_storage() {
     	// $file_path = join(DIRECTORY_SEPARATOR, array(DRUPAL_ROOT, variable_get('amber_storage_location', 'sites/default/files/amber')));
-    	$file_path = "/tmp";
+
+    	$base_dir = wp_upload_dir();
+    	$subdir = Amber::get_option('amber_storage_location', 'amber');
+    	$file_path = join(DIRECTORY_SEPARATOR, array($base_dir['basedir'], $subdir));
     	$storage = new AmberStorage($file_path);
 	  	return $storage;
 	}
@@ -119,7 +122,7 @@ class Amber {
 	  // Assume that we only have one cache of the data. This would need to change if we start tracking multiple caches
 	  if (isset($summaries['default']['location'],$summaries['default']['date'],$summaries['default']['size']) &&
 	      ($summaries['default']['size'] > 0)) {
-	    $result['data-cache'] = join(" ",array(get_site_url() . $summaries['default']['location'], date('c',$summaries['default']['date'])));
+	    $result['data-cache'] = join(" ",array(join("/", array(get_site_url(),$summaries['default']['location'])), date('c',$summaries['default']['date'])));
 	  } else {
 	    return $result;
 	  }
@@ -268,6 +271,108 @@ class Amber {
   		}
 	}
 
+	/**
+	 * Retrieve an item from the cache for display
+	 * @param $id string identifying the item to return
+	 * @return null|string
+	 */
+	private static function retrieve_cache_item($id) {
+	  $storage = Amber::get_storage();
+	  $data = $storage->get($id);
+	  $metadata = $storage->get_metadata($id);
+	  $status = Amber::get_status();
+	  $status->save_view($id);
+	  return ($data && $metadata) ? array('data' => $data, 'metadata' => $metadata) : NULL;
+	}
+
+	/**
+	 * Retrieve an asset from the cache for display
+	 * @param $id string identifying the item to return
+	 * @return null|string
+	 */
+	private function retrieve_cache_asset($cache_id, $asset_id) {
+	  $storage =  Amber::get_storage();
+	  // $d = $storage->get_asset($id, join('/',$args) . ($_SERVER['QUERY_STRING'] ? '?' . $_SERVER['QUERY_STRING'] : ''));
+	  $d = $storage->get_asset($cache_id, $asset_id );
+	  if ($d) {
+	    $data['data'] = $d;
+	    // Set the mime-type for certain files
+	    $last_element = $asset_id;
+	    $extension = substr($last_element, strrpos($last_element, '.') + 1);
+	    switch ($extension) {
+	      case "css" : $data['metadata']['type'] = 'text/css'; break;
+	      case "jpg" : $data['metadata']['type'] = 'image/jpeg'; break;      
+	      case "svg" : $data['metadata']['type'] = 'image/svg+xml'; break;      
+	    }
+	  }
+	  return (isset($data)) ? $data : NULL;
+	}
+
+
+	/* Rewrite rules to direct requests for cached content */
+	public static function add_rewrite_rules() {
+
+		// TODO : Make this work, and do not rely on editing .htaccess
+
+		// add_rewrite_rule(
+		//     '^.*amber/cache/([a-f0-9]+)/?$',
+		//     "/index.php?amber_cache=$matches[1]",
+		//     "index.php",
+		//     'top'
+		// );
+		// 		flush_rewrite_rules();
+
+//  		print "<pre>";
+// global $wp_rewrite;
+// print_r($wp_rewrite);
+// 		print "</pre>";
+
+		// =====================
+		// TODO : Move this to Activation ONLY
+		// =====================	
+		// flush_rewrite_rules();
+		// die();
+	}
+
+	/* Ensure that parameters passed by add_rewrite_rules() are accessible */
+	public static function custom_query_vars($vars) {
+		$vars[] = 'amber_cache';
+		$vars[] = 'amber_asset';
+		return $vars;
+	}
+
+	public static function display_cached_content ($wp) {
+
+		$cache_id = !empty($wp->query_vars['amber_cache']) ? $wp->query_vars['amber_cache'] : "";
+		$asset_id = !empty($wp->query_vars['amber_asset']) ? $wp->query_vars['amber_asset'] : "";
+		$asset_id = rtrim($asset_id,"/"); /* Get rid of stray characters on the end */
+
+		if (!empty($cache_id)) {
+			if (empty($asset_id)) {
+				/* This is the root item */
+				$data = Amber::retrieve_cache_item($cache_id);
+			    if (isset($data['metadata']['type'])) {
+			    	header('Content-Type', $data['metadata']['type']);
+			    }
+		    	print $data['data'];
+				die();
+			} else {
+				/* This is an asset */
+				$data = Amber::retrieve_cache_asset($cache_id, $asset_id);
+			    if (isset($data['metadata']['type'])) {
+			    	header('Content-Type', $data['metadata']['type']);
+				}
+		    	print($data['data']);
+				die();
+			}
+		}
+	}
+
+	public static function add_header()
+	{
+		// header( 'X-bogus: sfasdfas' );
+	}
+
 }
 
 include_once dirname( __FILE__ ) . '/amber-install.php';
@@ -291,6 +396,13 @@ add_action( 'wp_enqueue_scripts', array('Amber', 'register_plugin_assets') );
 
 /* Scan content for links whenever it's saved */
 add_action( 'save_post', array('Amber', 'extract_links') );
+
+/* Add actions and filters for loading cache content */
+add_action( 'init', array('Amber', 'add_rewrite_rules') );
+add_filter( 'query_vars', array('Amber', 'custom_query_vars') );
+add_action( 'parse_request', array('Amber', 'display_cached_content') );
+add_action( 'send_headers', array('Amber', 'add_header') );
+
 
 /* Add "Cache Now" link to links */
 add_filter( 'post_row_actions', array('Amber', 'add_post_row_actions'), 10, 2 );
