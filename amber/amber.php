7b43dd7a54dcb0b44b4933fbbc0ca8c9907c105f
jeffreylicht <jeffreylicht@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-09-23 22:09:57 +0000

    Rename github repositories
    git-svn-id: http://plugins.svn.wordpress.org/amberlink/trunk@1501479 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- amber/amber.php
+++ amber/amber.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Amber
- * Plugin URI: https://github.com/berkmancenter/robustness_wordpress
+ * Plugin URI: https://github.com/berkmancenter/amber_wordpress
  * Description: Enables the preservation of content to which your website links.
  * Version: 0.1
  * Author: Berkman Center for Internet & Society
